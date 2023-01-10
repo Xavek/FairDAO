3c8678bbf27bac34f043e1b2bef039e83ee5589e
shabbiryk.eth <shabbirkhand.sk@gmail.com>
2023-01-10 23:15:30 +0530

    Updated: the token supply

--- StandardDATA/contracts/GovernanceToken.sol
+++ StandardDATA/contracts/GovernanceToken.sol
@@ -6,12 +6,18 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 // Define the GovernanceToken contract, which is a subclass of ERC20Votes
 contract GovernanceToken is ERC20Votes {
-// Define a public variable to store the maximum supply of the token
-uint256 public s_maxSupply = 1000000000000000000000000;
 
-// Constructor function is called when the contract is deployed
-constructor() ERC20("GovernanceToken", "GT") ERC20Permit("GovernanceToken") {
-// Call the _mint function from the base contract to mint the maximum supply of the token to the contract creator
-_mint(msg.sender, s_maxSupply);
-}
+// Define a public constant variable to store the maximum supply of the token
+//using constant for maximumSupply makes it more readable
+// and it will save you from mutating this variable.
+// Furthermore, using `maxSupply` is more semantic than using `s_maxSupply`.
+
+uint256 public constant maxSupply = 10000000000000000000000;
+
+  // Constructor function is called when the contract is deployed
+  constructor() ERC20("GovernanceToken", "GT") ERC20Permit("GovernanceToken") {
+    // Call the _mint function from the base contract to mint the maximum supply of the token to the contract creator
+    // This will mint the maxSupply amount of tokens to msg.sender
+    _mint(msg.sender, maxSupply);
+  }
 }
